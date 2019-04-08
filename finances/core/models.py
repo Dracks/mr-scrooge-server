3ffa72d894097c984c91c8258130690f245f6e68
Jaume Singla Valls <jaume.singlavalls@gmail.com>
2019-04-08 14:04:14 +0200

    Little pylint fixes

--- finances/core/models.py
+++ finances/core/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
 class AbstractRawDataSource(models.Model):
-    movement_name=models.CharField(max_length=255)
-    date=models.DateField()
-    date_value=models.DateField(null=True)
-    details=models.TextField(null=True, blank=True)
-    value=models.FloatField()
+    movement_name = models.CharField(max_length=255)
+    date = models.DateField()
+    date_value = models.DateField(null=True)
+    details = models.TextField(null=True, blank=True)
+    value = models.FloatField()
 
     def __str__(self):
-        return "m:{} d:{} v:{} dd:{}$".format(self.movement_name, self.date, self.value, self.details)
+        return "m:{} d:{} v:{} dd:{}$".format(
+            self.movement_name, 
+            self.date, 
+            self.value, 
+            self.details
+        )
 
     class Meta:
         abstract = True
+
 
 class RawDataSource(AbstractRawDataSource):
     kind = models.CharField(max_length=255)
